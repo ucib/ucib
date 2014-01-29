9601cf1810b9904aa20caf0dd4208b58e53a9ec3
Matt Palmer <mpalmer@hezmatt.org>
2014-01-30 08:41:31 +1100

    Move the apt-file host check to the right place

--- plugins/host/debian/init.sh
+++ plugins/host/debian/init.sh
@@ -1,4 +1,11 @@
 if [ -e /etc/debian_version ]; then
+	if ! apt-file --help >/dev/null 2>&1; then
+		# apt-file is presumably not available
+		warning "apt-file is not installed; this isn't a fatal error, but it
+			will mean I can't automatically install programs that are
+			needed to build the image."
+	fi
+
 	install_program_in_host() {
 		local program="$1"
 		
--- plugins/os/wheezy/tasks/00-check-os-wheezy-programs.sh
+++ plugins/os/wheezy/tasks/00-check-os-wheezy-programs.sh
@@ -1,9 +1,2 @@
-if ! apt-file --help >/dev/null 2>&1; then
-	# apt-file is presumably not available
-	warning "apt-file is not installed; this isn't a fatal error, but it
-	        will mean I can't automatically install programs that are
-	        needed to build the image."
-fi
-
 check_program_available "debootstrap --help" "debootstrap"
 check_program_available "shred --help" "shred"
