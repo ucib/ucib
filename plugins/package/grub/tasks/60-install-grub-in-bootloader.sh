1776a2ae96180af4aec5146fca9bae74ed4ef186
Matt Palmer <mpalmer@hezmatt.org>
2015-05-26 08:34:14 +1000

    Support GRUB 2.0 proper

--- plugins/package/grub/tasks/60-install-grub-in-bootloader.sh
+++ plugins/package/grub/tasks/60-install-grub-in-bootloader.sh
@@ -20,9 +20,9 @@ if run_in_target grub-install --version | grep -q "1\\.99"; then
 	run_in_target grub-mkimage -d /usr/lib/grub/i386-pc -O i386-pc    \
 	   --output=/boot/grub/core.img --prefix="$prefix"                \
 	   biosdisk ext2 part_msdos
-		  
+
 	run_in_target grub-setup -d /boot/grub --root-device='(hd0)' "$BLOCK_DEVICE"
-		  
+
 	# Final, real device.map for boot
 	echo "(hd0) /dev/vda" >"${TARGET}/boot/grub/device.map"
 
@@ -40,32 +40,37 @@ if run_in_target grub-install --version | grep -q "1\\.99"; then
 	esac
 
 	echo "GRUB_DEVICE=$grub_device" >> "${TARGET}/etc/default/grub"
+elif run_in_target grub-install --version | grep -q "2\\."; then
+	run_in_target grub-install /dev/loop0
+
+	# Final, real device.map for boot
+	echo "(hd0) /dev/vda" >"${TARGET}/boot/grub/device.map"
 elif run_in_target grub-install --version | grep -q "0\\.97"; then
 	# Oh *man*... I thought grub2 was weird
 
 	register_cleanup cleanup_grub1_install
-	
+
 	# http://ebroder.net/2009/08/04/installing-grub-onto-a-disk-image/
 	# gave me the nasty details of this one
-	
+
 	blocks=$(($(optval image-size) * 2097152))
 	maj_num=$((0x$(stat -c %t "$BLOCK_DEVICE")))
 	min_num=$((0x$(stat -c %T "$BLOCK_DEVICE")))
-	
+
 	echo "0 $blocks linear $maj_num:$min_num 0" | dmsetup create hda
 	kpartx -a /dev/mapper/hda
-	
+
 	# Setup some dummy files
 	echo "(hd0) /dev/mapper/hda" >"${TARGET}/boot/grub/device.map"
 	echo "/dev/mapper/hda1 / ext4 defaults 0 0" >"${TARGET}/etc/mtab"
-	
+
 	run_in_target /sbin/grub-install /dev/mapper/hda >/dev/null 2>&1
-	
+
 	# Replace with a real device.map
 	echo "(hd0) /dev/vda" >"${TARGET}/boot/grub/device.map"
 	rm -f "${TARGET}/etc/mtab"
 else
 	fatal "Unknown version of GRUB installed.  Bailing."
-fi	
+fi
 
 info "GRUB installed"
