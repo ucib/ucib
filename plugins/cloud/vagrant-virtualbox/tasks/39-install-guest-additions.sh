d0478351ad596c8811f43644f82dacb9e88311b3
Matt Palmer <mpalmer@hezmatt.org>
2015-08-07 18:00:06 +1000

    Fix the virtualbox plugin install process
    All sorts of changes here.  It's all very embarrassment.

--- plugins/cloud/vagrant-virtualbox/tasks/39-install-guest-additions.sh
+++ plugins/cloud/vagrant-virtualbox/tasks/39-install-guest-additions.sh
@@ -3,33 +3,18 @@ wget --progress=dot -O "${WORKSPACE}/vbox.iso" http://download.virtualbox.org/vi
 # I really don't want to clog up the rest of UCIB with this stuff, since I doubt any other
 # targets are going to require development packages
 if [ -e "${TARGET}/etc/debian_version" ]; then
-	target_kernel_ver="$(run_in_target dpkg -l linux-image-*-* | grep ^ii | cut -d ' ' -f 3 | sed 's/^linux-image-//')"
-
-	if [ -z "$target_kernel_ver" ]; then
-		fatal "No kernel version found.  Something is very wrong here."
-	fi
-
-	headers_pkg="linux-headers-${target_kernel_ver}"
-	run_in_target apt-get -y install build-essential dkms $headers_pkg |& spin "Installing build environment"
-
-	# This is exceedingly fucked up... VirtualBox provides no capability
-	# to build the modules it needs for a kernel other than what is
-	# currently running, and even if you do manage to hack around that,
-	# it tries to immediately load them into the running kernel, which
-	# you can *guarantee* is going to fail spectacularly.  The only way
-	# I can work out to get around it is to just skip the build, ignore the
-	# attempt to load the module, and pray like fuck it works on next boot.
-	#
-	# Annoyingly, this means that I've got to leave the kernel headers,
-	# build-essential, and the rest of it all installed on the image,
-	# making it *significantly* larger.
-	cat <<-EOF >"${TARGET}/usr/local/sbin/dkms"
-#!/bin/sh
-
-exit 0
-fi
-EOF
-	chmod 0555 "${TARGET}/usr/local/sbin/dkms"
+	case "${OPTS[arch]}" in
+		"amd64")
+			install_packages_in_target linux-headers-amd64
+			;;
+		"i386")
+			install_packages_in_target linux-headers-i686
+			;;
+		*)
+			fatal "Unknown architecture: ${OPTS[arch]}"
+	esac
+elif [ -e "${TARGET}/etc/redhat-release" ]; then
+	install_packages_in_target kernel-devel perl gcc
 fi
 
 mkdir "${TARGET}/vbox"
@@ -43,9 +28,9 @@ cleanup_mount_vbox_iso() {
 
 register_cleanup "cleanup_mount_vbox_iso"
 
-# This is supposed to fail
+# This is supposed to fail, because installing the additions *requires* the
+# current kernel to be the one you want to build against.  The proper build
+# will happen on first boot, because we add a haxxed up vboxadd init script.
 run_in_target "/vbox/VBoxLinuxAdditions.run" |& spin "Installing VBox Guest Additions" || true
 
-if [ -e "${TARGET}/etc/debian_version" ]; then
-	rm -f "${TARGET}/usr/local/sbin/dkms"
-fi
+cp $(plugin_file cloud/vagrant-virtualbox init-scripts/vboxadd) "${TARGET}/etc/init.d/"
