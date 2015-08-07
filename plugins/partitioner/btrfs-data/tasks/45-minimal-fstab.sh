7a82bf169c9c78f2d01a4f676e0950c5794055a6
Matt Palmer <mpalmer@hezmatt.org>
2015-08-07 17:54:24 +1000

    Call the correct function to get the partition for /data

--- plugins/partitioner/btrfs-data/tasks/45-minimal-fstab.sh
+++ plugins/partitioner/btrfs-data/tasks/45-minimal-fstab.sh
@@ -1,2 +1,2 @@
 echo "$(disk_partition 1) / ext4 defaults 0 1" >>${TARGET}/etc/fstab
-echo "$(dist_partition 2) /data btrfs defaults 0 2" >>${TARGET}/etc/fstab
+echo "$(disk_partition 2) /data btrfs defaults 0 2" >>${TARGET}/etc/fstab
