f73eccd6c337c9e70fd4ca3af76ce935dd66dc81
Matt Palmer <mpalmer@hezmatt.org>
2015-08-07 17:59:04 +1000

    Fix a couple of problems with the centos6 bootstrap process
    rinse is not my best friend right now.  It installs *every* package in
    its cache, even if those packages are now horribly out-of-date, which
    tends to cause all manner of fun and games.  So, we turn caching off.
    It also leaves the system in a horrendously broken state, with brokne
    dependencies all over the place.  So, we've got to go in and manually
    fix that shit up.

--- plugins/os/centos6/tasks/20-bootstrap.sh
+++ plugins/os/centos6/tasks/20-bootstrap.sh
@@ -5,11 +5,21 @@ fi
 if ! rinse  \
            --arch "${OPTS[arch]}" \
            --distribution centos-6 \
+           --cache 0 \
            "$mirroropt" \
            --directory "$TARGET" |&
-               tee "${WORKSPACE}/rinse_output" |
                spin "Running bootstrap"; then
-	error "Bootstrap failed:"
-	cat "${WORKSPACE}/rinse_output"
+	fatal "Bootstrap failed"
 	exit 1
 fi
+
+# OMFG rinse is terribad... it leaves all sorts of stuff broken.  So,
+# we need to fix the metadata cache, and then fix all the stuff that
+# was left broken.
+run_in_target yum clean metadata >/dev/null 2>&1
+
+((run_in_target yum check dependencies || true) \
+	| grep 'has missing requires' \
+	| sed 's/.\+has missing requires of //' \
+	| sed 's/ [=<>].\+//' \
+	| while read dep; do install_packages_in_target "$dep"; done) |& spin "Fixing the bootstrapped base image"
