a95e832d27017f9e6fe52be39070e5ce1c96d4d0
Matt Palmer <mpalmer@hezmatt.org>
2015-08-07 17:54:59 +1000

    Make the btrfs filesystem without newer, shinier options
    Old kernels dislike newer btrfs filesystems.

--- plugins/partitioner/btrfs-data/tasks/16-format-data-partition.sh
+++ plugins/partitioner/btrfs-data/tasks/16-format-data-partition.sh
@@ -1,3 +1,11 @@
 debug "Formatting /data filesystem as btrfs..."
 
-mkfs.btrfs -f "${PARTITIONS[/data]}" |& spin "Formatting '/data' filesystem"
+# Sigh... mkfs.btrfs turned on options that make the damned thing unrecognisable
+# to older kernels.  YOU PEOPLE ARE MONSTERS
+set +o pipefail
+if mkfs.btrfs -h 2>&1 | grep -q -- -O; then
+	btrfs_opts="-O ^extref"
+fi
+set -o pipefail
+
+mkfs.btrfs $btrfs_opts -f "${PARTITIONS[/data]}" |& spin "Formatting '/data' filesystem"
